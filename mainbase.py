54736bbad218e82630960709571c266532265d88
HansB33X <97372552+HansB33X@users.noreply.github.com>
2022-02-20 15:23:46 +0700

    Update mainbase.py

--- mainbase.py
+++ mainbase.py
@@ -3,8 +3,6 @@ import os
 import pyfiglet
 
 def main():
-        os.system('pip install pyfiglet')
-        os.system('clear')
 	logo = pyfiglet.figlet_format("BASE64")
 	print(logo)
 	print("decode / encode by HansB33X\n")
